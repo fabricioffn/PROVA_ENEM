09292c8994db8380cdc53541fa51557e60621b1b
Fabricio <fnmamao@gmail.com>
2020-02-23 20:13:13 -0300

    mariobross

--- Program.cs
+++ Program.cs
@@ -172,6 +172,8 @@ namespace prova_enem
                     Console.WriteLine(nomeAluno + " congratulations my little friend");
                     continuar = false;
                     Console.WriteLine("Digite Enter para salvar sua prova e sair de férias");
+Console.Beep(659, 125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(523, 125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(375); Console.Beep(392, 125); Thread.Sleep(375); Console.Beep(523, 125); Thread.Sleep(250); Console.Beep(392, 125); Thread.Sleep(250); Console.Beep(330, 125); Thread.Sleep(250); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(466, 125); Thread.Sleep(42); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(392, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(880, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(587, 125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(250); Console.Beep(392, 125); Thread.Sleep(250); Console.Beep(330, 125); Thread.Sleep(250); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(466, 125); Thread.Sleep(42); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(392, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(880, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(587, 125); Console.Beep(494, 125); Thread.Sleep(375); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(698, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(698, 125); Thread.Sleep(625); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(622, 125); Thread.Sleep(250); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(523, 125); Thread.Sleep(1125); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(698, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(698, 125); Thread.Sleep(625); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(622, 125); Thread.Sleep(250); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(523, 125);
+
                     Console.ReadLine();
                 }
                 else
@@ -197,7 +199,57 @@ namespace prova_enem
                     Thread.Sleep(50);
                     Console.Beep(350, 250);
                     Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    
+                    Console.Beep(300, 500);
                     Thread.Sleep(50); 
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(250, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(350, 250);
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(250, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(350, 250);
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50); 
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50); 
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(250, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(350, 250);
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(250, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(350, 250);
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50); 
+                    
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50); 
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(250, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(350, 250);
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(250, 500);
+                    Thread.Sleep(50);
+                    Console.Beep(350, 250);
+                    Console.Beep(300, 500);
+                    Thread.Sleep(50);  
                     
                     Console.WriteLine("Você quer tentar realizar a prova novamente? Digite S ou N");
                     var respostaContinuaProva = Console.ReadLine();
